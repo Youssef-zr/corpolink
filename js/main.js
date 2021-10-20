559415054632d96c01ee42e896d1afe4e7c83a84
Youssef-zr <yn-neinaa@hotmail.com>
2021-10-20 02:16:17 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,38 @@
+$(() => {
+
+    $(window).on('scroll', function () {
+        let $s_stats = $('.section-stats')
+        if ($("html,body").scrollTop() > 1800 && !$s_stats.hasClass('finished')) {
+
+            $('.count').each(function () {
+                $(this).prop('Counter', 0).animate({
+                    Counter: $(this).text()
+                }, {
+                    duration: 1000,
+                    easing: 'swing',
+                    step: function (now) {
+                        $(this).text(Math.ceil(now));
+                        $s_stats.addClass('finished')
+                    }
+                });
+            });
+        }
+    })
+
+    let $social_media = $('._team')
+
+    $social_media.on('mouseenter', function () {
+        $(this).find('.social-media-links,.overlay').animate({
+            width: "100%",
+            height: "100%"
+        }, 500);
+    })
+    $social_media.on('mouseleave', function () {
+        $(this).find('.social-media-links,.overlay').animate({
+            width: 0,
+            height: 0
+        }, 500);
+    })
+
+
+})
